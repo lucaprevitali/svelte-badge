c4b78297dab2aa2f8f28052fe28be9ea9edff198
Luca Previtali <lucaprevi@gmail.com>
2022-03-11 08:11:43 +0100

    increase package version

--- rollup.config.js
+++ rollup.config.js
@@ -6,7 +6,7 @@ const pkg = require("./package.json");
 export default {
   input: "src/Badge.svelte",
   output: [
-    { file: pkg.module, format: "en" },
+    { file: pkg.module, format: "es" },
     { file: pkg.main, format: "umd", name: "Badge" },
   ],
   plugins: [svelte(), resolve()],
